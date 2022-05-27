ad3208b8f730b522df8e1ada77e370a89772467c
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-05-27 16:48:50 -0400

    Starter Files

---
+++ script.js
@@ -0,0 +1,34 @@
+//Get elements
+var months = $('.months')
+var days = $(".days")
+var hours = $(".hours")
+
+var nextMonth = [
+    "January", 
+    "February", 
+    "March", 
+    "April", 
+    "May", 
+    "June", 
+    "July", 
+    "August", 
+    "September", 
+    "October", 
+    "November", 
+    "December"
+]
+
+for (i=0; i <= nextMonth.length, i++;){
+    debugger;
+    console.log(i)
+}
+var month = {
+    names: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
+    
+}
+var day = {
+    "nums": [1, 2, 3, 4, 5, 6, 7,],
+
+}
+
+m = moment()
