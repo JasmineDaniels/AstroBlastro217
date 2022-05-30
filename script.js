c67a5be3b1de87a28c60724c87458035123b89cc
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-05-29 22:02:11 -0400

    + hours & styling--html/css

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-//Get elements
-var months = $('.months')
-var days = $(".days")
-var hours = $(".hours")
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do, YYYY, h:mm a"))
+console.log(today)
 
-var nextMonth = [
-    "January", 
-    "February", 
-    "March", 
-    "April", 
-    "May", 
-    "June", 
-    "July", 
-    "August", 
-    "September", 
-    "October", 
-    "November", 
-    "December"
-]
+var currentTime = today.format("h:mm a")
+console.log(currentTime)
 
-for (i=0; i <= nextMonth.length, i++;){
-    debugger;
-    console.log(i)
-}
-var month = {
-    names: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
-    
-}
-var day = {
-    "nums": [1, 2, 3, 4, 5, 6, 7,],
-
-}
-
-m = moment()
+// var newHour = today.add(1, "h");
+// console.log(newHour)
