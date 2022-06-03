835459b396a643034b2ffb46159848f2678eb9c7
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-06-03 19:36:46 -0400

    local storage returned to DOM --js

--- script.js
+++ script.js
@@ -23,21 +23,73 @@ for (let i = 9; i < 18; i++) {
     };
 }
 
-// Local Storage User Input => saveBtn textarea.val() 
+// When $(this) saveBtn is clicked,
+//User Input => saveBtn textarea.val() 
 
 $(".saveBtn").click(function(){
     var value = $(this).siblings(".textArea").val();
     var key = $(this).attr("id");
-    var output = $(this).siblings("textArea")
 
     if (key && value){
         localStorage.setItem(key, value);
         console.log(localStorage)
     }
 
-    output.append("<li>" +localStorage.getItem("key") + "</li>")
 });
 
-// $("text-9").append(localStorage.getItem("key"))
+if (localStorage.getItem("key-9")){
+    $("#text-9").append(localStorage.getItem("key-9"));
+} else {
+    $("#text-9").append("Add Text");
+};
+
+if (localStorage.getItem("key-10")){
+    $("#text-10").append(localStorage.getItem("key-10"));
+} else {
+    $("#text-10").append("Add Text");
+};
+
+if (localStorage.getItem("key-11")){
+    $("#text-11").append(localStorage.getItem("key-11"));
+} else {
+    $("#text-11").append("Add Text");
+};
+
+if (localStorage.getItem("key-12")){
+    $("#text-12").append(localStorage.getItem("key-12"));
+} else {
+    $("#text-12").append("Add Text");
+};
+
+if (localStorage.getItem("key-13")){
+    $("#text-13").append(localStorage.getItem("key-13"));
+} else {
+    $("#text-13").append("Add Text");
+};
+
+if (localStorage.getItem("key-14")){
+    $("#text-14").append(localStorage.getItem("key-14"));
+} else {
+    $("#text-14").append("Add Text");
+};
+
+if (localStorage.getItem("key-15")){
+    $("#text-15").append(localStorage.getItem("key-15"));
+} else {
+    $("#text-15").append("Add Text");
+};
+
+if (localStorage.getItem("key-16")){
+    $("#text-16").append(localStorage.getItem("key-16"));
+} else {
+    $("#text-16").append("Add Text");
+};
+
+if (localStorage.getItem("key-17")){
+    $("#text-17").append(localStorage.getItem("key-17"));
+} else {
+    $("#text-17").append("Add Text");
+};
+
 
 
